39db8d6e9192fd6bf844b6eceae5634a33ee0e1e
mrbrianjiang1121 <byjiang@outlook.com>
2019-06-09 16:29:20 -0500

    Created a new initial Xcode Project

---
+++ SwiftBasics/SwiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Brian Jiang on 6/9/19.
+//  Copyright © 2019 Brian Jiang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
